9a693eb7005d45b3e5d00e1178046014e2932992
Ghian Bersani <agenciaonedev@gmail.com>
2024-06-28 16:05:22 -0300

    Menssagem de ERROU

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ function verificarChute() {
       document.getElementById('reiniciar').removeAttribute('disabled');
     }else {
         if (chute > numeroSecreto){
-            exibirTextoNaTela('p', 'O número secreto é menor');
+            exibirTextoNaTela('p', 'Errou! O número secreto é menor');
         }else {
-            exibirTextoNaTela('p', 'O número secreto é maior');
+            exibirTextoNaTela('p', 'Errou! O número secreto é maior');
         }
         tentativas++;
         limparCampo()
